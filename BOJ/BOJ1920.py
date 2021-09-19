dbc7bca0c358fbeb3ee71d877b07ca3001fbdd89
An jaegyeong <74139727+anjaekk@users.noreply.github.com>
2021-09-19 12:33:15 +0900

    Update BOJ1920.py

--- BOJ/BOJ1920.py
+++ BOJ/BOJ1920.py
@@ -24,3 +24,22 @@ for target in search:
   start = 0
   end = len(arr)-1
   print(binary(target, arr, start, end))
+
+  
+  
+  #=========================================================================
+  
+  for i in mlist:
+  start = 0
+  end = len(nlist)-1
+  while start <= end:
+    mid = (start+end) // 2
+    if nlist[mid] == i:
+      print(1)
+      break
+    elif nlist[mid] > i:
+      end = mid-1
+    elif nlist[mid] < i:
+      start = mid+1
+  if start > end:
+    print(0)
