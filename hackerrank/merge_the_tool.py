9ef5e67155d5c88237f7631baf1489dffc17479a
An jaegyeong <74139727+anjaekk@users.noreply.github.com>
2022-11-02 22:03:44 +0900

    Create merge_the_tool.py

---
+++ hackerrank/merge_the_tool.py
@@ -0,0 +1,14 @@
+def merge_the_tools(string, k):
+    chunk = []
+    for i in range(0, len(string)):
+        chunk.append(string[i])
+        if len(chunk) == k:
+            list = []
+            for c in chunk:
+                if c not in list: list.append(c)
+            print(''.join(list))
+            chunk = []
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
