a7b576a24e76c738993f3ed3862c201fdd4d5886
An jaegyeong <74139727+anjaekk@users.noreply.github.com>
2022-11-03 22:43:06 +0900

    Create Default_Arguments.py

---
+++ hackerrank/Default_Arguments.py
@@ -0,0 +1,39 @@
+from typing import Type, Union
+
+
+class OddStream():
+    def __init__(self) -> None:
+        self.current = 1
+
+    def get_next(self) -> int:
+        result = self.current
+        self.current += 2
+        return result
+
+
+
+class EvenStream():
+    def __init__(self) -> None:
+        self.current = 0
+
+    def get_next(self) -> int:
+        result = self.current
+        self.current += 2
+        return result
+
+
+def print_from_stream(
+    n: int, 
+    stream_name: Union[Type[OddStream], Type[EvenStream]] = EvenStream()
+) -> None:
+    for _ in range(int(n)):
+        print(stream_name.get_next())
+
+
+q = int(input())
+for _ in range(q):
+    stream_name, n = input().split()
+    if stream_name == "odd":
+        print_from_stream(n, stream_name=OddStream())
+    else:
+        print_from_stream(n)
