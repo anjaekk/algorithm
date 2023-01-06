6aa5a148f1f03201e32f3537c8b87b7a804700b7
An jaegyeong <anjaekk@gmail.com>
2023-01-06 11:35:07 +0900

    Update binary_gap.py

--- others/binary_gap.py
+++ others/binary_gap.py
@@ -1,5 +1,12 @@
 # codility
 
 def solution(N):
-    binary = str(bin(N)[2:])
-    return 0 if "1" not in binary[1:] else len(max(binary.split("1")))
+    binary = bin(N)[2:]
+    if "1" not in binary[1:]:
+        return 0
+    last_one = 0
+    for i in range(len(binary)-1, 0, -1):
+        if binary[i] == "1":
+            last_one = i
+            break
+    return len(max(binary[:last_one].split("1")))
