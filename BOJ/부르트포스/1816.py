7234bdcc55f9898aa0cba0c7a5e8d76c36156f4d
An jaegyeong <anjaekk@gmail.com>
2023-08-27 23:16:37 +0900

    Create 1816.py

---
+++ BOJ/부르트포스/1816.py
@@ -0,0 +1,23 @@
+# 암호키(백준 1816번)
+# 1을 제외한 약수가 1,000,000보다 커야 성공
+
+
+from sys import stdin
+input = stdin.readline
+
+T = int(input())
+
+# 간소화
+# def validate_password(password):
+#    return next(("NO" for n in range(2, 1000000) if password % n == 0), "YES")
+
+def validate_password(password):
+    for n in range(2, 1000000):
+        if password % n == 0:
+            return "NO"
+    return "YES"
+
+
+for _ in range(T):
+    password = int(input())
+    print(validate_password(password))
